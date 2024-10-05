62ae4c86df0d56968610b3bc7da68999e0cd4c86
mukulpandey35 <mukulrajapandey@gmail.com>
2024-10-06 00:56:05 +0530

    Just Formatted

--- C++ DSA/Array/Char Array & Strings/reorganizeString.cpp
+++ C++ DSA/Array/Char Array & Strings/reorganizeString.cpp
@@ -1,48 +1,54 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-string reorganizeString(string s){
+string reorganizeString(string s)
+{
     int hash[26] = {0};
-    
-    for(int i =0; i<s.size();i++){
-        hash[s[i] -'a']++;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i] - 'a']++;
     }
-    //find the most occuring char 
+    // find the most occuring char
     char maxFreqChar;
     int maxFreq = INT32_MIN;
-    for(int i = 0;i<26;i++){
-        if(hash[i]> maxFreq){
-            maxFreq= hash[i];
-            maxFreqChar = i +'a';
+    for (int i = 0; i < 26; i++)
+    {
+        if (hash[i] > maxFreq)
+        {
+            maxFreq = hash[i];
+            maxFreqChar = i + 'a';
         }
-
     }
-    int index =0;
-    while (maxFreq > 0 && index < s.size()){
+    int index = 0;
+    while (maxFreq > 0 && index < s.size())
+    {
         s[index] = maxFreqChar;
-        maxFreq --;
+        maxFreq--;
         index += 2;
     }
 
-    if (maxFreq != 0){
+    if (maxFreq != 0)
+    {
         return "";
     }
-    hash[maxFreqChar - 'a'] =0;
-    for(int i =0 ;i<26;i++){
-        while (hash[i]>0){
-            index = index>= s.size() ? 1 : index;
-            s[index]= i+'a';
+    hash[maxFreqChar - 'a'] = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        while (hash[i] > 0)
+        {
+            index = index >= s.size() ? 1 : index;
+            s[index] = i + 'a';
             hash[i]--;
             index += 2;
         }
     }
     return s;
-   
-
 }
 
-int main(){
-    string s = "aaabccdef";
+int main()
+{
+    string s = "abcdef";
 
-    cout<<"The Output is : "<<reorganizeString(s)<<endl;
-} 
+    cout << "The Output is : " << reorganizeString(s) << endl;
+}
